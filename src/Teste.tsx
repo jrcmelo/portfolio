59b54fb10fdf7e6e64cde24d703b0ec45f28f798
John Robert <john.robert.c.melo@gmail.com>
2024-02-18 03:24:29 -0300

    Configurando e testando ambiente de desenvolvimento.

---
+++ src/Teste.tsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+const Botao = styled.button`
+  background-color: red;
+`
+
+function Teste() {
+  return <Botao>Olá Mundo!</Botao>
+}
+
+export default Teste
